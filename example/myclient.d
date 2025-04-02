2184649d05b1803532f620987abb1a2bf4c3d6ac
Elin Hellström <elin2001.hellstrom@gmail.com>
2025-04-02 15:56:15 +0200

    first commit

---
+++ example/myclient.d
@@ -0,0 +1,6 @@
+myclient.o: myclient.cc ../include/connection.h \
+  ../include/connectionclosedexception.h
+
+../include/connection.h:
+
+../include/connectionclosedexception.h:
---
+++ src/connection.d
@@ -0,0 +1,6 @@
+src/connection.o: src/connection.cc include/connection.h \
+  include/connectionclosedexception.h
+
+include/connection.h:
+
+include/connectionclosedexception.h:
